72cecd72228a390106a29bca98427817d2c9ae1a
João Pedro <jhonpitere163@gmail.com>
2025-04-13 13:05:21 -0400

    Envaindo arquivos

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,113 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
+import Spline from '@splinetool/react-spline'
 
 export default function Home() {
+  const [linguagens, setLinguagens] = useState({
+    projeto1: ['Html', 'Css', 'Js', 'Boostrap', 'Animate.css', 'Font Awesome', 'AOS Scroll'],
+    projeto2: ['Python', 'HTML', 'CSS', 'Boostrap', 'JavaScript ', 'yt-dlp', 'Flask'],
+    projeto3: ['Python', 'Django', 'PostgreSQL'],
+  });
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
+    <main className="relative min-h-screen text-white">
+      {/* Navbar */}
+      <nav className="absolute top-0 left-0 w-full flex items-center justify-between px-8 py-4 bg-black/60 backdrop-blur-sm z-10">
+        <h1 className="text-white text-xl font-semibold">JaooPredo</h1>
+        <ul className="flex space-x-6 text-white">
+          <li>
+            <a href="#projetos" className="hover:text-indigo-400 transition">
+              Projetos
+            </a>
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+          <li>
+            <a href="#contato" className="hover:text-indigo-400 transition">
+              Contato
+            </a>
           </li>
-        </ol>
+        </ul>
+      </nav>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Spline Hero Background */}
+      <div className="relative w-full h-[60vh] sm:h-[70vh] md:h-[80vh] lg:h-[90vh] overflow-hidden">
+        <div className="absolute inset-0">
+          <Spline
+            scene="https://prod.spline.design/RWW81iBj4ozJuUv6/scene.splinecode"
+            className="w-full h-full object-contain"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      </div>
+
+
+
+      {/* Seção de Projetos */}
+      <section id="projetos" className="bg-black py-16 px-6">
+        <h3 className="text-3xl font-bold text-center mb-12 text-white">Projetos</h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto relative">
+          {/* Card Container */}
+          <div className="flex flex-col items-center">
+            {/* Card 1 */}
+            <div className="mb-5 bg-transparent rounded-2xl shadow-[0_4px_30px_rgba(255,255,255,0.1)] backdrop-blur-md p-6 hover:shadow-[0_6px_40px_rgba(255,255,255,0.2)] transition transform hover:scale-105">
+              <h4 className="text-xl font-semibold mb-2 text-white">Ferrari F40</h4>
+              <p className="text-gray-400">
+                Projeto sobre a icônica Ferrari F40, com design radical, 324 km/h de velocidade e destaque histórico para entusiastas de performance.
+              </p>
+              {/* Linguagens */}
+              <div className="mt-2 mb-3">
+                <h5 className="text-white">Linguagens:</h5>
+                <p className="text-gray-400">
+                  {linguagens.projeto1.join(', ')}
+                </p>
+              </div>
+
+              <a href="https://jaopred0.github.io/Ferrari-F40/" target="bland_" className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition">
+                Acessar Site
+              </a>
+            </div>
+            
+            {/* Card 2 */}
+            <div className="mb-5 bg-transparent rounded-2xl shadow-[0_4px_30px_rgba(255,255,255,0.1)] backdrop-blur-md p-6 hover:shadow-[0_6px_40px_rgba(255,255,255,0.2)] transition transform hover:scale-105">
+              <h4 className="text-xl font-semibold mb-2 text-white">SoundJao</h4>
+              <p className="text-gray-400">
+              Baixe músicas e playlists via Flask com qualidade máxima, capas originais e interface responsiva.
+              </p>
+              {/* Linguagens */}
+              <div className="mt-2 mb-3">
+                <h5 className="text-white">Linguagens:</h5>
+                <p className="text-gray-400">
+                  {linguagens.projeto2.join(', ')}
+                </p>
+              </div>
+
+              <a href="https://github.com/JaoPred0/SoundJao" target="bland_" className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition">
+                Acessar projeto
+              </a>
+            </div>
+          </div>
+          <Spline
+            scene="https://prod.spline.design/HEh5dP-AOjCEfZ0O/scene.splinecode"
+          />
+
+          {/* Card Container
+          <div className="flex flex-col items-center">
+
+          </div> */}
+        </div>
+      </section>
+      <hr />
+      <section id="contato" className="py-20 bg-black text-white text-center">
+        <h2 className="text-4xl font-bold mb-6">Entre em Contato</h2>
+        <p className="text-gray-400 mb-10">Tem um projeto em mente? Vamos conversar no WhatsApp!</p>
+
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://wa.me/5599999999999?text=Olá%2C%20vi%20seu%20portfólio%20e%20gostaria%20de%20falar%20sobre%20um%20projeto!"
           target="_blank"
           rel="noopener noreferrer"
+          className="inline-block bg-green-500 hover:bg-green-600 transition px-6 py-3 rounded-full text-white font-medium shadow-lg hover:scale-105"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          Falar no WhatsApp
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </section>
+
+    </main>
+  )
 }
